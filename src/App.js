7da44cbee48f270234640a4da27bc480fb8bb190
sarahkydnc <sarah.kydnc@gmail.com>
2022-06-24 16:57:27 +0800

    Created and loaded ButtonBox component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Wrapper from "./components/Wrapper";
 import Screen from "./components/Screen";
+import ButtonBox from "./components/ButtonBox";
 import "./App.css";
 
 const App = () => {
   return (
     <Wrapper>
       <Screen value="0" />
+      <ButtonBox></ButtonBox>
     </Wrapper>
   );
 };
