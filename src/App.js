e3baccd56f2a609e886c2dd5a04e0312fdb07fad
sarahkydnc <sarah.kydnc@gmail.com>
2022-06-27 14:51:51 +0800

    Rendered all calculator buttons

--- src/App.js
+++ src/App.js
@@ -4,18 +4,31 @@ import ButtonBox from "./components/ButtonBox";
 import Button from "./components/Button";
 import "./App.css";
 
+const btnValues = [
+  ["C", "+-", "%", "/"],
+  [7, 8, 9, "x"],
+  [4, 5, 6, "-"],
+  [1, 2, 3, "+"],
+  [0, ".", "="],
+];
+
 const App = () => {
   return (
     <Wrapper>
       <Screen value="0" />
       <ButtonBox>
-        <Button
-          className=""
-          value="0"
-          onClick={() => {
-            console.log("Button clicked!");
-          }}
-        />
+        {btnValues.flat().map((btn, i) => {
+          return (
+            <Button
+              key={i}
+              className={btn === "=" ? "equals" : ""}
+              value={btn}
+              onClick={() => {
+                console.log(`${btn} clicked!`);
+              }}
+            />
+          );
+        })}
       </ButtonBox>
     </Wrapper>
   );
